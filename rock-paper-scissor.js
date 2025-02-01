9a26865206ad84b044795fb3e735cfa8154faf36
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-02-01 22:25:50 +0530

    Basic UI code added

---
+++ rock-paper-scissor.js
@@ -0,0 +1,11 @@
+function handleRockClick(){
+    document.getElementById('result').innerHTML = "You Choose Rock";
+}
+
+function handlePaperClick(){
+    document.getElementById('result').innerHTML = "You Choose Papper";
+}
+
+function handleScissorClick(){
+    document.getElementById('result').innerHTML = "You Choose Scissor";
+}
