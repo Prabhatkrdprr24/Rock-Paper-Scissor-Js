08166ca61fd745d0475a8a81bca8613145411efc
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-02-01 23:38:10 +0530

    random value generation code added

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,11 +1,48 @@
 function handleRockClick(){
-    document.getElementById('result').innerHTML = "You Choose Rock";
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+    let computerChoiceText = "";
+    if(computerChoice == 1){
+        computerChoiceText = "rock";
+    }
+    else if(computerChoice == 2){
+        computerChoiceText = "Paper";
+    }
+    else{
+        computerChoiceText = "Scissor";
+    }
+
+    document.getElementById('result').innerHTML = `You Choose Rock.<br></nr> Computer Choose ${computerChoiceText}`;
 }
 
 function handlePaperClick(){
-    document.getElementById('result').innerHTML = "You Choose Papper";
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+    let computerChoiceText = "";
+    if(computerChoice == 1){
+        computerChoiceText = "rock";
+    }
+    else if(computerChoice == 2){
+        computerChoiceText = "Paper";
+    }
+    else{
+        computerChoiceText = "Scissor";
+    }
+
+    document.getElementById('result').innerHTML = `You Choose Paper. <br>Computer Choose ${computerChoiceText}`;
 }
 
 function handleScissorClick(){
-    document.getElementById('result').innerHTML = "You Choose Scissor";
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+    let computerChoiceText = "";
+    if(computerChoice == 1){
+        computerChoiceText = "rock";
+    }
+    else if(computerChoice == 2){
+        computerChoiceText = "Paper";
+    }
+    else{
+        computerChoiceText = "Scissor";
+    }
+
+    document.getElementById('result').innerHTML = `You Choose Scissor.<br> Computer Choose ${computerChoiceText}`;
+
 }
